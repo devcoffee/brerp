a3ad976ae7cf778acc317b3045c3a07f965e91b6
Jean Pierre <pierre_e6@hotmail.com>
2019-02-28 12:54:25 -0300

    Create alter_fillfactor.sql
    add  fillfactor

---
+++ alter_fillfactor.sql
@@ -0,0 +1,10 @@
+ALTER TABLE brerp.ad_recentitem    			     SET (fillfactor = 50);
+ALTER TABLE brerp.ad_sequence      			     SET (fillfactor = 50);
+ALTER TABLE brerp.t_inventoryvalue 			     SET (fillfactor = 50);
+ALTER TABLE brerp.m_productprice 			       SET (fillfactor = 50);
+ALTER TABLE brerp.t_trialbalance 		         SET (fillfactor = 50);
+ALTER TABLE brerp.t_cof_relatoriocompra 	   SET (fillfactor = 50);
+ALTER TABLE brerp.ad_recentitem 			       SET (fillfactor = 50);
+ALTER TABLE brerp.m_storagereservation       SET (fillfactor = 50);
+ALTER TABLE brerp.cof_c_taxdetermination_hdr SET (fillfactor = 50);
+ALTER TABLE brerp.gl_journal 				         SET (fillfactor = 50);
